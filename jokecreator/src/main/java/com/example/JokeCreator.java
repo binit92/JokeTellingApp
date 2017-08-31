dc1beb291bc85ad20a7a4b3cbc45f27951244ff2
binit92 <kumar.binit1992@gmail.com>
2017-08-31 11:20:37 +0530

    Step 2: Create an Java Library

---
+++ jokecreator/src/main/java/com/example/JokeCreator.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class JokeCreator {
+
+    public static String getJoke(){
+        return " Roses are Red, Violets are Blue. Who is your daddy? And what does he do ? ";
+    }
+}
